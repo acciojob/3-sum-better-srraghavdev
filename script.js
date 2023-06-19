316348b105260f9a64e9f24699bde410bb4b4fad
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-19 23:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function threeSum(arr, target) {
-  //your code here
-  let ans = Infinity;
-  arr.sort((a, b) => a - b);
-  arr.forEach((a, i) => {
-    const target2 = target - a;
-    let l = i + 1;
-    let r = arr.length - 1;
-    while (l < r) {
-      if (Math.abs(target - (arr[l] + arr[r] + a)) <= Math.abs(target - ans)) {
-        ans = arr[l] + arr[r] + a;
-      }
-      if (arr[l] + arr[r] > target2) r--;
-      else l++;
-    }
-  });
-  return ans;
-
-
+let ans=0;
+arr.sort(function(a,b){return a-b})
+for(let i=0;i<arr.length;i++){
+	let a= arr[i]
+	let left=i+1
+	let right=arr.length-1
+	while(left<right){
+		if(Math.abs(target-ans)>Math.abs(a+arr[left]+arr[right])){
+			ans=a+arr[left]+arr[right]
+		}
+		if(a+arr[left]+arr[right]>target){
+			right--
+		}
+		if(a+arr[left]+arr[right]<target)){
+			left++
+		}
+	}
+}
+	return ans
 }
 
 module.exports = threeSum;
